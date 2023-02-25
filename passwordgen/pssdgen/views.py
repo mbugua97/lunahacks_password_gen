d27c0b657c343018794d29adfc8040cfee6b6239
mbugua <jobugua97@gmail.com>
2023-02-25 17:26:05 +0300

    new files

--- passwordgen/pssdgen/views.py
+++ passwordgen/pssdgen/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 def homepage(request):
     if request.method=='POST':
         password=request.get['name']
-    return render(request,"pssd\index.html")
+    return render(request,"pssdgen\index.html")
