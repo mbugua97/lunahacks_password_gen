995bfb55ca36cccee4ca33ec5dd1e1ea3dcd06e2
mbugua <jobugua97@gmail.com>
2023-02-25 18:07:06 +0300

    added the hashing mechanism

--- passwordgen/pssdgen/views.py
+++ passwordgen/pssdgen/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+import hashlib
+
+
+hash_object = hashlib.md5()
+
 
 def homepage(request):
     if request.method=='POST':
         password=request.POST['pass']
+        hash_object.update(password.encode())
+        hashed_psd = hash_object.hexdigest()
+        context={'hashed_psd':hashed_psd}
+
         print(password)
-    return render(request,"pssdgen\index.html")
+    return render(request,"pssdgen\index.html",context)
